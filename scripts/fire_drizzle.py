0010977bfcbe633af1d0f16a0a43c2458c8cbd94
Miles Currie <mhc13@my.fsu.edu>
2018-07-25 13:53:57 -0400

    calculates horizontal component

--- scripts/fire_drizzle.py
+++ scripts/fire_drizzle.py
@@ -13,9 +13,38 @@ from matplotlib import colors
 correction = True
 
 def init_grid(len_side):
-    grid = np.ones((len_side, len_side))
+    state_grid = np.ones((len_side, len_side))
+    bucket_grid = np.zeros((len_side, len_side))
 
-    return grid
+    state_grid = np.load('polka.npy')
+
+    # start a point fire
+    state_grid[160, 50] = 2
+    bucket_grid[160, 50] = 1.
+
+    # start a line fire
+    state_grid[160, 140:160] = 2
+    bucket_grid[160, 140:160] = 1
+
+    # block
+    # state_grid[98:104, 48:52] = 0
+
+    return bucket_grid, state_grid
+
+
+def init_elev_grid(len_side):
+    # gaussian mountain:
+    x, y = np.meshgrid(np.linspace(-100,100,200), np.linspace(-100,100,200))
+    d = np.sqrt(x*x+y*y)
+    sigma, mu = 30.0, 1
+    elev_grid = np.exp(-((d-mu)**2 / (2.0 * sigma**2)))
+    elev_grid *= 10
+    elev_grid = elev_grid.astype(int)
+
+
+    # flat
+    elev_grid = np.zeros((len_side, len_side))
+    return elev_grid
 
 
 def rate_of_spread_xy(R0, Ebar, alpha, theta, ti):
@@ -80,6 +109,34 @@ def rate_of_spread(R0, U_eq, alpha, phi, ti):
     return ros
 
 
+def horizontal_component(elev_grid_neighbors):
+
+    neighbor_angles = [[3*np.pi/4, np.pi/2, np.pi/4],
+                       [np.pi, 0, 0],
+                       [5*np.pi/4, 3*np.pi/2, 7*np.pi/4]]
+
+    pix_of_interest = elev_grid_neighbors[1, 1]
+
+    delt_elev_grid = elev_grid_neighbors - pix_of_interest
+
+    delt_x_grid = np.array([[-1, 0, 1],
+                            [-1, 0, 1],
+                            [-1, 0, 1]])
+
+    delt_y_grid = np.array([[1, 1, 1],
+                            [0, 0, 0],
+                            [-1, -1, -1]])
+
+    x_ang_grid = np.tan(delt_elev_grid / delt_x_grid)
+
+    y_ang_grid = np.tan(delt_elev_grid / delt_y_grid)
+
+    h_correction = np.sqrt(np.cos(neighbor_angles)**2 * np.cos(x_ang_grid)**2
+                           + np.sin(neighbor_angles)**2 * np.cos(y_ang_grid)**2)
+
+    return h_correction
+
+
 if __name__ == '__main__':
 
     # ###### cmap #######
@@ -94,13 +151,13 @@ if __name__ == '__main__':
     U_eq = 0.012
     R0 = 0.5
 
-    timesteps = 90
-    len_side = 120
+    timesteps = 400
+    len_side = 200
     alpha = np.pi - np.pi/4
 
     y_start = 80
 
-    bucket_grid = np.zeros((len_side, len_side))
+    # bucket_grid = np.zeros((len_side, len_side))
     neighbor_angles = np.arange(0, 2*np.pi, np.pi/4)
     neighbor_locations = np.array([(+1, 0), (+1, +1), (0, +1), (-1, +1),
                                    (-1, 0), (-1, -1), (0, -1), (+1, -1)])
@@ -108,7 +165,7 @@ if __name__ == '__main__':
                                    1, np.sqrt(2),
                                    1, np.sqrt(2),
                                    1, np.sqrt(2)])
-    bucket_grid[y_start, len_side/2] = 1.
+    # bucket_grid[y_start, len_side/2] = 1.
 
     Ueq_grid = U_eq*np.ones((len_side, len_side))
     R0_grid = R0*np.ones((len_side, len_side))
@@ -118,15 +175,16 @@ if __name__ == '__main__':
 
     alpha_grid[len_side/2:, :] = np.pi
 
-    state_grid = np.ones_like(bucket_grid)
-    state_grid[y_start, len_side/2] = 2
+    elev_grid = init_elev_grid(len_side)
 
+    # state_grid = np.ones_like(bucket_grid)
+    # state_grid[y_start, len_side/2] = 2
+    bucket_grid, state_grid = init_grid(len_side)
     plt.figure()
     plt.imshow(state_grid, cmap=fire_cmap, norm=norm)
     plt.axis('off')
     plt.colorbar()
     plt.savefig('../plots/im000.png')
-
     for ti in range(timesteps):
         print 'working on timestep', ti
         I_inds_x, I_inds_y = np.where(state_grid == 2)
@@ -134,22 +192,33 @@ if __name__ == '__main__':
             if np.all(state_grid[I_inds_x[n]-1:I_inds_x[n]+2,
                                  I_inds_y[n]-1:I_inds_y[n]+2] != 1):
                 state_grid[I_inds_x[n], I_inds_y[n]] = 3
-
             else:
+                elev_neighbors = elev_grid[I_inds_x[n]-1:I_inds_x[n]+2,
+                                           I_inds_y[n]-1:I_inds_y[n]+2]
+
+                h_correction = horizontal_component(elev_neighbors)
                 # OLD WAY
                 for neighbor_num, phi in enumerate(neighbor_angles):
                     ros = rate_of_spread(R0_grid[I_inds_x[n], I_inds_y[n]],
                                          Ueq_grid[I_inds_x[n], I_inds_y[n]],
                                          alpha_grid[I_inds_x[n], I_inds_y[n]],
                                          phi, ti)
-                    bucket_grid[I_inds_x[n]
-                                + neighbor_locations[neighbor_num, 0],
-                                I_inds_y[n]
-                                + neighbor_locations[neighbor_num, 1]] \
-                        += (ros / neighbor_distances[neighbor_num])
-                    new_I_inds_x, new_I_inds_y = np.where((bucket_grid >= 1.)
-                                                          & (state_grid != 3))
-                    state_grid[new_I_inds_x, new_I_inds_y] = 2
+
+                    ros *= h_correction[neighbor_locations[neighbor_num, 0],
+                                        neighbor_locations[neighbor_num, 1]]
+
+                    try:
+                        bucket_grid[I_inds_x[n]
+                                    + neighbor_locations[neighbor_num, 0],
+                                    I_inds_y[n]
+                                    + neighbor_locations[neighbor_num, 1]] \
+                            += (ros / neighbor_distances[neighbor_num])
+                        new_I_inds_x, new_I_inds_y = np.where((bucket_grid >= 1.)
+                                                              & (state_grid ==
+                                                                 1))
+                        state_grid[new_I_inds_x, new_I_inds_y] = 2
+                    except IndexError:
+                        pass
                 '''
 
                 # NEW WAY, vectorized
